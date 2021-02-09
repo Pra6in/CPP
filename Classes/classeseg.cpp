217574df0e31af781b8ff1eb41a55b741a64f5be
prabin-acharya@ <acharyaprabin101@gmail.com>
2021-02-09 23:59:09 +0545

    added basic examples of classes

---
+++ Classes/classeseg.cpp
@@ -0,0 +1,22 @@
+// classes example
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+  public:
+    void set_values (int,int);
+    int area() {return width*height;}
+};
+
+void Rectangle::set_values (int x, int y) {   //Here the scope operator is used in the definition of function 
+  width = x;                                  //set_values to define a member of a class outside the class itself.
+  height = y;
+}
+
+int main () {
+  Rectangle rect;
+  rect.set_values (3,4);
+  cout << "area: " << rect.area();
+  return 0;
+}
